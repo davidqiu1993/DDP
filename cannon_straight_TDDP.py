Fix some calculation bugs in DDP toolset and update the cannon examples.

cannon_straight_TDDP.py
@@ -7,7 +7,6 @@ import pdb
 if __name__ == '__main__':
   desired_destination_x = 5.0
   desired_destination_y = 2.0
-  lr = 0.0025
 
   # node: start
   node_start = DynamicsSystemNode('start', next_primitive_name='shoot')
@@ -17,40 +16,64 @@ if __name__ == '__main__':
   # node: destination
   node_destination = DynamicsSystemNode('destination', prev_primitive_name='shoot')
 
-  # dynamics: shoot (start -> destination)
-  dynamics_shoot = DynamicsSystemEdgeDynamicsModel()
+  # transition: shoot
+  trans_shoot = DynamicsSystemTransitionModel()
 
-  def dynamics_shoot_dynamics_func (ssa):
+  def trans_shoot_transition_func (ssa):
+    prob = { 'destination': 1.0 }
+    return prob
+  trans_shoot.transition_func = trans_shoot_transition_func
+
+  def trans_shoot_transition_dfunc (ssa):
+    d = {}
+    T = trans_shoot_transition_func(ssa)
+    for b in T:
+      d[b] = {}
+      for k in ssa.keys():
+        if k == 'selection':
+          d[b][k] = np.zeros((1))
+        else:
+          d[b][k] = np.zeros((ssa.retrive(k).shape[0]))
+    return d
+  trans_shoot.transition_dfunc = trans_shoot_transition_dfunc
+
+  # edge dynamics: shoot -> destination
+  edge_shoot_destination = DynamicsSystemEdgeDynamicsModel()
+
+  def edge_shoot_destination_dynamics_func (ssa):
     next_ssa = ssa.copy()
     next_ssa.updateSelection(None)
     destination_x = desired_destination_x
     destination_y = (destination_x - ssa.retrive('shooter')[0]) * np.sin(ssa.retrive('shoot_angle')[0]) + ssa.retrive('shooter')[1]
     next_ssa.updateStateElement('destination', np.array([destination_x, destination_y]))
     return next_ssa
-  dynamics_shoot.dynamics_func = dynamics_shoot_dynamics_func
+  edge_shoot_destination.dynamics_func = edge_shoot_destination_dynamics_func
 
-  def dynamics_shoot_dynamics_dfunc (ssa):
-    next_ssa = dynamics_shoot_dynamics_func(ssa)
+  def edge_shoot_destination_dynamics_dfunc (ssa):
+    next_ssa = edge_shoot_destination_dynamics_func(ssa)
     d = {}
     for next_ssa_elem in next_ssa.keys():
       d[next_ssa_elem] = {}
       for ssa_elem in ssa.keys():
         if next_ssa_elem == 'selection' or ssa_elem == 'selection':
           d[next_ssa_elem][ssa_elem] = np.zeros((1, 1))
+        elif next_ssa_elem == ssa_elem:
+          assert(ssa.retrive(ssa_elem).shape[0] == next_ssa.retrive(next_ssa_elem).shape[0])
+          d[next_ssa_elem][ssa_elem] = np.eye(ssa.retrive(ssa_elem).shape[0])
         else:
           d[next_ssa_elem][ssa_elem] = np.zeros((ssa.retrive(ssa_elem).shape[0], next_ssa.retrive(next_ssa_elem).shape[0]))
     d_destx_theta = 0
     d_desty_theta = (desired_destination_x - ssa.retrive('shooter')[0]) * np.cos(ssa.retrive('shoot_angle')[0])
     d['destination']['shoot_angle'] = np.array([[d_destx_theta, d_desty_theta]])
     return d
-  dynamics_shoot.dynamics_dfunc = dynamics_shoot_dynamics_dfunc
+  edge_shoot_destination.dynamics_dfunc = edge_shoot_destination_dynamics_dfunc
 
-  def dynamics_shoot_reward_func (next_ssa):
+  def edge_shoot_destination_reward_func (next_ssa):
     r = - (next_ssa.retrive('destination')[1] - desired_destination_y) ** 2
     return r
-  dynamics_shoot.reward_func = dynamics_shoot_reward_func
+  edge_shoot_destination.reward_func = edge_shoot_destination_reward_func
 
-  def dynamics_shoot_reward_dfunc (next_ssa):
+  def edge_shoot_destination_reward_dfunc (next_ssa):
     d = {}
     for k in next_ssa.keys():
       if k == 'selection':
@@ -61,14 +84,21 @@ if __name__ == '__main__':
     d_desty = - 2 * (next_ssa.retrive('destination')[1] - desired_destination_y)
     d['destination'] = np.array([d_destx, d_desty])
     return d
-  dynamics_shoot.reward_dfunc = dynamics_shoot_reward_dfunc
+  edge_shoot_destination.reward_dfunc = edge_shoot_destination_reward_dfunc
+
+  # primitive: shoot
+  primitive_shoot = DynamicsSystemPrimitive('shoot', alpha=0.0025)
+  primitive_shoot.prev_node_name = node_start.name
+  primitive_shoot.transition = trans_shoot
+  primitive_shoot.dynamics_dict = {}
+  primitive_shoot.dynamics_dict[node_destination.name] = edge_shoot_destination
 
   # dynamics system
-  dynamics_system = LinearDynamicsSystem()
+  dynamics_system = TreeDynamicsSystem()
   dynamics_system.updateNode(node_start.name, node_start)
   dynamics_system.updateNode(node_destination.name, node_destination)
   dynamics_system.updateRootNodeName(node_start.name)
-  dynamics_system.updateDynamics('shoot', 'start', dynamics_shoot, 'destination', alpha=lr)
+  dynamics_system.updatePrimitive(primitive_shoot.name, primitive_shoot)
 
   # execute
   last_J = None